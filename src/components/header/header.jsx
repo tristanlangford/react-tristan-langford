5145c93c6505f996aa3ef0bbd4a8a157e60e0a16
Tristan Langford <tristanlangford@hotmail.com>
2020-08-08 18:24:58 +0100

    all functionality

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,13 +3,13 @@ import './header.css'
 export default function Header() {
   return (
     <header>
-    <nav class="navbar.navbar-expand-lg.navbar-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
       <a class="navbar-brand" href='/'>Tristan Langford</a> 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse.navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav.mr-auto">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a class="nav-link" href="/portfolio">Portfolio</a>
           </li>
