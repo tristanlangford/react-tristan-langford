a9858bbfb8187e51bd76b1b8cbc8a94f187b553c
Tristan Langford <tristanlangford@hotmail.com>
2020-08-08 17:12:55 +0100

    added header & footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="footer"> 
+    <table> 
+      <tbody> 
+        <tr> 
+          <td> <a href="/images/TRISTAN_LANGFORD_CV.pdf" download="download"><img class="footer-logo" src="/images/PDF-Mark3.png"/></a></td>
+          <td> <a href="https://github.com/tristanlangford"><img class="footer-logo" src="/images/GitHub-Mark.png"/></a></td>
+          <td> <a href="https://www.linkedin.com/in/tristan-langford-61711a70/"><img class="footer-logo" src="/images/LinkedIn-Mark.png"/></a></td>
+          <td> <a href="mailto:tristanlangford@hotmail.com"><img class="footer-logo" src="/images/Email-Mark.png"/></a></td>
+        </tr>
+      </tbody>
+    </table>
+  </footer>
+    );
+}
---
+++ src/components/header/header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+    <nav class="navbar.navbar-expand-lg.navbar-light">
+      <a class="navbar-brand" href='/'>Tristan Langford</a> 
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse.navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav.mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/experience">Experience</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    </header>
+    );
+}
