637006b278743219e3f8203f7a8196e32992645a
Tristan Langford <tristanlangford@hotmail.com>
2020-08-09 13:30:26 +0100

    rel="noopener"

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -6,10 +6,10 @@ export default function Contact() {
     <div className="container contact">
         <ul>
             <li> 
-                Email: <a className="contact-link" href="mailto:tristanlangford@hotmail.com" target="_blank">tristanlangford@hotmail.com</a>
+                Email: <a className="contact-link" href="mailto:tristanlangford@hotmail.com" rel="noopener">tristanlangford@hotmail.com</a>
             </li>
             <li> 
-                LinkedIn: <a className="contact-link" href="https://www.linkedin.com/in/tristan-langford-61711a70/" target="_blank">Tristan Langford</a>
+                LinkedIn: <a className="contact-link" href="https://www.linkedin.com/in/tristan-langford-61711a70/" rel="noopener">Tristan Langford</a>
             </li>
             <li> 
                 Github: <a className="contact-link" href="https://github.com/tristanlangford" target="_blank">tristanlangford</a>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -11,8 +11,8 @@ export default function Footer() {
                 <img className="footer-logo" src="/images/PDF-Mark3.png" alt="pdf logo"/>
               </a>
           </td>
-          <td> <a href="https://github.com/tristanlangford" target="_blank"><img className="footer-logo" src="/images/GitHub-Mark.png" alt="github logo"/></a></td>
-          <td> <a href="https://www.linkedin.com/in/tristan-langford-61711a70/" target="_blank"><img className="footer-logo" src="/images/LinkedIn-Mark.png" alt="linkedIn logo"/></a></td>
+          <td> <a href="https://github.com/tristanlangford" target="_blank" rel="noopener"><img className="footer-logo" src="/images/GitHub-Mark.png" alt="github logo"/></a></td>
+          <td> <a href="https://www.linkedin.com/in/tristan-langford-61711a70/" target="_blank" rel="noopener"><img className="footer-logo" src="/images/LinkedIn-Mark.png" alt="linkedIn logo"/></a></td>
           <td> <a href="mailto:tristanlangford@hotmail.com"><img className="footer-logo" src="/images/Email-Mark.png" alt="email logo"/></a></td>
         </tr>
       </tbody>
