4a954914b4f45a985e801fc589c238c55fbae78a
Tristan Langford <tristanlangford@hotmail.com>
2020-08-09 17:46:30 +0100

    added Strava link

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -37,8 +37,8 @@ export default function Home() {
                     <p>Following a 5-year career in the medical-devices industry, inspired by the growing focus on technology, I joined Makers Academy, London's leading programming bootcamp, learning best practice, tools and methodologies.</p>
                     <ul> 
                         <li><strong>Dulwich Hamlet</strong> - On a Saturday can be found standing in the "rabble" supporting the best team in South London, Dulwich Hamlet FC</li>
-                        <li><strong>Cricket</strong> - On a Sunday I'll be playing for one of the worst cricket teams in London formerly officially the worst</li>
-                        <li><strong>Running</strong> - To keep myself sane and busy during lockdown, I got into running. So far I've completed a couple half marathons. </li>
+                        <li><strong>Cricket</strong> - On a Sunday I'll be playing for one of the worst cricket teams in London (formerly officially the worst)</li>
+                        <li><strong>Running</strong> - To keep myself sane and busy during lockdown, I got into running. So far I've completed a couple half marathons. Follow me on <a className="strava" href="https://www.strava.com/athletes/15342123" target="_blank" rel="noopener noreferrer">Strava</a></li>
                     </ul>
                     </div>
                     <img src={"/images/TL_image5.png"} className="tristan-image" alt="tristan"/>
