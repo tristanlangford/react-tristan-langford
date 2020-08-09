86521fffe133cffccd31c7e3f6abee52e94b8877
Tristan Langford <tristanlangford@hotmail.com>
2020-08-09 12:33:20 +0100

    links open in new tab

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -6,10 +6,10 @@ export default function Contact() {
     <div className="container contact">
         <ul>
             <li> 
-                Email: <a className="contact-link" href="mailto:tristanlangford@hotmail.com">tristanlangford@hotmail.com</a>
+                Email: <a className="contact-link" href="mailto:tristanlangford@hotmail.com" target="_blank">tristanlangford@hotmail.com</a>
             </li>
             <li> 
-                LinkedIn: <a className="contact-link" href="https://www.linkedin.com/in/tristan-langford-61711a70/">Tristan Langford</a>
+                LinkedIn: <a className="contact-link" href="https://www.linkedin.com/in/tristan-langford-61711a70/" target="_blank">Tristan Langford</a>
             </li>
             <li> 
                 Github: <a className="contact-link" href="https://github.com/tristanlangford">tristanlangford</a>
