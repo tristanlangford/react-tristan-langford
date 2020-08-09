3dee89545b5227986bb98c26c4712b1fae814f8c
Tristan Langford <tristanlangford@hotmail.com>
2020-08-09 09:41:32 +0100

    removed unused requires

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './index.css'
