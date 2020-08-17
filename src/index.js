e5aa52600bbae3a831fd17d4bdf7c38de6778a31
Tristan Langford <tristanlangford@hotmail.com>
2020-08-17 10:51:03 +0100

    refactored

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './index.css'
 import App from './App';
 import Footer from './components/footer/footer'
 import Header from './components/header/header'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
