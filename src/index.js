b67f721565df54bd30fa2e04284d2c36479cad12
Tristan Langford <tristanlangford@hotmail.com>
2020-08-17 10:47:51 +0100

    removed unused files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import App from './App';
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import './index.css'
-import App from './App';
-import Header from './components/header/header'
-import Footer from './components/footer/footer'
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,7 +22,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
